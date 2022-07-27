7124f4e7c8916de4cd092042dc31973edc727644
andres-v-flores <andres.v.flores@accenture.com>
2022-07-27 18:58:25 +0800

    init commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,196 @@
-import logo from './logo.svg';
+import { useState,useCallback, useEffect, useRef } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Button } from 'react-bootstrap';
 import './App.css';
+import DisplayToDo from './DisplayToDo';
+import ToDoForm from './ToDoForm';
+
+const URL = 'https://react-http-398cc-default-rtdb.asia-southeast1.firebasedatabase.app/task';
+
 
 function App() {
+  const inputRef = useRef();
+  const [toDo, setToDo] = useState([]);
+  const [err, setErr] = useState(null);
+  const [isEdit, setIsEdit] = useState(false)
+  const [inputChange, setInputChange] = useState('');
+  const [tasked, setTasked] = useState('');
+
+  const inputChangeHandler = (task) => {
+    setInputChange(task)
+  }
+  const saveEditTask = async() => {
+    console.log(tasked + ' edited task ' + inputChange)
+    let newTaskObj = {task: inputChange}
+
+    async function fetchPut() {
+      const response = await fetch(URL+'/'+tasked+'.json',{
+        method: 'PUT',
+        body: JSON.stringify(newTaskObj),
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      });
+      if(!response.ok){
+        throw new Error('Something went wrong on editing')
+      }
+      const data = await response.json();
+      return data
+    }
+    try{
+      let data = await fetchPut();
+      console.log(data);
+      let arrIndex = toDo.findIndex(obj => obj.id === tasked)
+      setToDo(prev => {
+        prev[arrIndex] = {...newTaskObj, id: tasked};
+        return prev;
+      })
+      console.log(toDo[arrIndex].task);
+      setIsEdit(false);
+      setTasked('');
+      setInputChange('');
+    } catch (e) {
+      setErr(e.message)
+    }
+    
+  }
+  
+  const fetchEditTask = (taskId, task) => {
+    console.log("editing the " + taskId);
+    setInputChange(task)
+    setIsEdit(true)
+    setTasked(taskId)
+    inputRef.current.focus();
+  }
+
+  const submitToDataBase = async (obj) => {
+    
+    //console.log(obj);
+    //setToDo(prev => [...prev, obj]); 
+    async function fetchPost (obj) {
+      const response = await fetch(URL + '.json', {
+        method: 'POST',
+        body: JSON.stringify(obj),
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      }) // do a post request
+  
+      if(!response.ok){
+        throw new Error('Something went wrong with posting to database')
+      } // if error throw erro
+      const data = await response.json();
+      return data; // else return data {name: -asdfiwerqwerasdf}
+    }
+
+    try{ // try to send data
+      const data = await fetchPost(obj);
+      setToDo(prev => [...prev, {...obj, id: data.name}]) // if success update state
+      console.log(data);
+    } catch (e) { // if failed dont set state
+      setErr(e.message)
+      console.log('soemthing went error')
+    }
+  }
+  //above is the posting logic
+
+  // const removeItemFromArr = (indexNum) =>{
+  //   setToDo(prev => prev.pull(indexNum))
+  // }
+
+  const fetchTask = useCallback(async () => {
+    async function fetchGet() {
+      const response = await fetch(URL + '.json'); // get data
+      if(!response.ok){
+        throw new Error('Something went wrong with fetching from database')
+      }
+      const data = await response.json();
+      //console.log(data);
+      return data;
+    }
+    
+    try{ // try to fetch data then update state to display item
+      let data = await fetchGet();
+      let taskArr = [];
+      for (const key in data){
+        taskArr.push({
+          id: key,
+          task: data[key].task,
+        })
+      }
+      setToDo(taskArr);
+      console.log(data);
+      console.log('fetch success')
+    } catch (e) { // failed do something else
+      setErr(e.message);
+      console.log('soemthing went error')
+    }
+    
+  }, [])
+
+  const deleteTask = async(taskId) => {
+    console.log(taskId);
+    console.log(toDo);
+    async function fetchDel(){
+      const response = await fetch(URL+'/'+taskId+'.json',{
+        method: 'DELETE',
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      });
+      if(!response.ok){
+        throw new Error ('Something went wrong when deleting data from data base')
+      }
+      console.log(response);
+      return null
+    }
+  
+    try {
+      await fetchDel();
+      let newArr = toDo.filter(object => object.id !== taskId)
+      setToDo(newArr);
+    } catch (e) {
+      setErr(e.message);
+    }
+    
+  }
+
+const fetchButtonHandler = () => {
+  fetchTask();
+}
+
+  useEffect(()=>{
+    fetchTask();
+  },[fetchTask]) // since fetchTask wont change will only run init
+
+  const toDoArr = toDo.map((toDoItem )=> {
+  return (<DisplayToDo 
+    className={'list-group-item '} 
+    key={toDoItem.id} 
+    id={toDoItem.id} 
+    toDo={toDoItem.task} 
+    onDelete={deleteTask} 
+    onEditTask={fetchEditTask} />)} )
+  // above is the fetching logic
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <ToDoForm 
+        focusRef={inputRef} 
+        onSubmitForm={submitToDataBase} 
+        inputChange={inputChange}
+        setInputChange={inputChangeHandler}
+        isEdit={isEdit}
+        onDoneEdit={saveEditTask}
+        />
+        <div className='w-50'>
+          <Button onClick={fetchButtonHandler}>Fetch task</Button>
+          <ul className="list-group  red">
+                {toDoArr}
+          </ul>
+        </div>
+
       </header>
     </div>
   );
---
+++ src/classComps/AppClass.js
@@ -0,0 +1,207 @@
+import React, {Component, createRef} from "react";
+import {Button} from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '../App.css';
+import DisplayToDoClass from './DisplayToDoClass';
+import ToDoFormClass from './ToDoFormClass';
+
+const URL = 'https://react-http-398cc-default-rtdb.asia-southeast1.firebasedatabase.app/task';
+
+class AppClass extends Component {
+    constructor(props){
+        super(props);
+        this.inputRef = createRef();
+        this.state = {
+            toDo: [],
+            err: null,
+            isEdit: false,
+            inputChange: '',
+            tasked: '',
+        };
+        this.inputChangeHandler = this.inputChangeHandler.bind(this);
+        this.fetchEditTask = this.fetchEditTask.bind(this);
+        this.fetchButtonHandler = this.fetchButtonHandler.bind(this);
+        this.fetchTask = this.fetchTask.bind(this);
+        this.submitToDataBase = this.submitToDataBase.bind(this);
+        this.deleteTask = this.deleteTask.bind(this);
+        this.saveEditTask = this.saveEditTask.bind(this);
+    }
+    inputChangeHandler(task){
+        this.setState({inputChange: task});
+    }
+    fetchEditTask(taskId, task) {
+        console.log("editing the " + taskId);
+        this.setState({
+            inputChange: task,
+            isEdit: true,
+            tasked: taskId
+        })
+        this.inputRef.current.focus();
+    }
+
+    fetchButtonHandler() {
+        this.fetchTask();
+    }
+
+    async fetchTask(){
+        async function fetchGet() {
+            const response =await fetch(URL + '.json'); 
+            if(!response.ok){
+                throw new Error('Something went wrong with getting data from DB');
+            }
+            const data = await response.json();
+            return data;
+        }
+        try{
+            let data = await fetchGet();
+            let taskArr = [];
+            for (const key in data){
+                taskArr.push({
+                    id: key,
+                    task: data[key].task,
+                })
+            }
+            this.setState({toDo: taskArr});
+            // console.log(data);
+            // console.log('fetch success');
+        } catch(e) {
+            this.setState({err: e.message});
+            console.log('something went error')
+        }
+    }
+    async submitToDataBase(obj) {
+        async function fetchPost (obj) {
+            const response = await fetch(URL + '.json', {
+                method: "POST",
+                body: JSON.stringify(obj),
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            })
+
+            if(!response.ok){
+                throw new Error('Something went wrong in posting to DB');
+            }
+            const data = await response.json();
+            return data;
+        }
+
+        try{
+            const data = await fetchPost(obj);
+            this.setState(prev => {
+                return {toDo: [...prev.toDo, {...obj, id: data.name}]};
+            })
+            // console.log(data);
+        } catch (e) {
+            this.setState({err: e.message});
+            console.log('something went error')
+        }
+    }
+    async deleteTask(taskId) {
+        // console.log(taskId);
+        // console.log(this.state.toDo);
+        async function fetchDel() {
+            const response = await fetch(URL + '/' + taskId + '.json', {
+                method: 'DELETE',
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            });
+            if(!response.ok){
+                throw new Error ('Something went wrong when deleting from DB');
+            }
+            // console.log(response);
+            return null;
+        }
+        try{
+            await fetchDel();
+            this.setState(prev => {
+                return {toDo: prev.toDo.filter(obj => obj.id !== taskId)}
+            });
+        } catch (e) {
+            this.setState({err: e.message});
+        }
+    }
+    async saveEditTask() {
+        console.log(this.state.tasked + ' edited task ' + this.state.inputChange);
+        let newTaskObj = {task: this.state.inputChange};
+        async function  fetchPut(){
+            const response = await fetch(URL + '/'+ this.state.tasked+'.json', {
+                method: 'PUT',
+                body: JSON.stringify(newTaskObj),
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            });
+            if(!response.ok){
+                throw new Error('Somehting went wrong on editing');
+            }
+            const data = await response.json();
+            return data;
+        }
+
+        try{
+            console.log('trying to do put')
+            let data = await fetchPut(); // class crashes here IDK why
+            // double checked it but no spelling errors
+            // will just crash
+            // other functionalities work just fine
+            console.log(data);
+            let arrIndex = this.state.toDo.findIndex(obj => obj.id === this.state.tasked);
+            this.setState(prev => {
+                prev.toDo[arrIndex] = {...newTaskObj, id: prev.tasked};
+                return {
+                    toDo: prev.toDo,
+                    isEdit: false,
+                    tasked: '',
+                    inputChange: '',
+                };
+            })
+        } catch (e) {
+            console.log('something went wrong');
+            console.log(this.state);
+            console.log(e.message)
+            this.setState({err: e.message});
+        }
+    }
+
+    componentDidMount(){
+        this.fetchTask();
+    }
+
+    render(){
+        let toDoArr = this.state.toDo.map((toDoItem) => {
+            return(<DisplayToDoClass
+                className={'list-group-item '}
+                key={toDoItem.id}
+                id={toDoItem.id}
+                toDo={toDoItem.task}
+                onDelete={this.deleteTask}
+                onEditTask={this.fetchEditTask}
+                />)
+        })
+
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <ToDoFormClass
+                    focusRef={this.inputRef}
+                    onSubmitForm={this.submitToDataBase}
+                    inputChange={this.state.inputChange}
+                    setInputChange={this.inputChangeHandler}
+                    isEdit={this.state.isEdit}
+                    onDoneEdit={this.saveEditTask}
+                    />
+                    <div className="w-50">
+                        <Button onClick={this.fetchButtonHandler}>Fetch Task</Button>
+                        <ul className="list-group red">
+                            {toDoArr}
+                        </ul>
+                    </div>
+                </header>
+            </div>
+        )
+    }
+}
+
+export default AppClass;
